054389d518ceb0c27338bf430f5494b1ceedcb5f
Carlos Buitrago <bhca0629@yahoo.es>
2024-06-08 21:40:57 -0500

    pinecone and embeddings

--- src/app.py
+++ src/app.py
@@ -1,12 +1,21 @@
-from fastapi import FastAPI, Request, File, UploadFile
+from fastapi import FastAPI, Request, File, UploadFile, HTTPException
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
-from src.helpers import respond_to_message
 from fastapi.middleware.cors import CORSMiddleware
 from src.logger import create_logger
 import os
+from src.helpers import (
+    respond_to_message,
+    load_docs,
+    create_pinecone_index,
+    create_embeddings,
+    add_embeddings_to_pinecone
+)
+
+from dotenv import load_dotenv
+load_dotenv()
 
 app = FastAPI()
 
@@ -24,7 +33,7 @@ logger.info("Static files mounted")
 templates = Jinja2Templates(directory="templates")
 logger.info("Templates loaded")
 
-
+# Create a directory to store uploaded files
 upload_dir = "./uploads"
 os.makedirs(upload_dir, exist_ok=True)
 
@@ -39,12 +48,41 @@ async def index(request: Request):
 
 @app.post("/upload")
 async def upload_files(files: list[UploadFile] = File(...)):
-    for file in files:
-        file_location = os.path.join(upload_dir, file.filename)
-        with open(file_location, "wb") as f:
-            f.write(file.file.read())
+    try:
+        for file in files:
+            file_location = os.path.join(upload_dir, file.filename)
+            with open(file_location, "wb") as f:
+                f.write(file.file.read())
+
+            # Load the document --TODO
+                documents = load_docs(file_location)
+                if documents is None:
+                    logger.error("Error loading document")
+                    raise HTTPException(status_code=400, detail="Error loading document")
             
-    return JSONResponse(content={"message": "Files successfully uploaded"})
+            # create pinecone index
+            try: 
+                index_name = os.getenv("PINECONE_INDEX_NAME")
+                pinecone_key = os.getenv("PINECONE_API_KEY")
+                create_pinecone_index(index_name, pinecone_key)
+                try:
+                    # create embeddings and add to pinecone
+                    namespace = os.getenv("PINECONE_NAMESPACE")
+                    embeddings = create_embeddings(documents)
+                    add_embeddings_to_pinecone(index_name, embeddings, namespace)
+                except Exception as e:
+                    logger.error(f"Error adding embeddings to Pinecone index: {str(e)}")
+                    raise HTTPException(status_code=500, detail="Error adding embeddings to Pinecone index")
+
+            except Exception as e:
+                logger.error(f"Error creating Pinecone index: {str(e)}")
+                raise HTTPException(status_code=500, detail="Error creating Pinecone index")
+
+
+
+        return {"filename": file.filename, "content": documents}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
 
 
 @app.get("/files")
@@ -57,7 +95,7 @@ async def list_files():
 async def send_message(message: Message):
     # Process the incoming message
     user_message = message.message
-    bot_response = respond_to_message(user_message, "veterinarian")
+    bot_response = respond_to_message(user_message, "formal")
     return JSONResponse(content={"message": bot_response})
 
 
--- src/helpers.py
+++ src/helpers.py
@@ -7,7 +7,6 @@ from langchain_community.document_loaders import UnstructuredPDFLoader
 from langchain_community.document_loaders import OnlinePDFLoader
 from src.prompt_templates import basic_template, formal_template
 
-from .logger import create_logger
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -24,7 +23,74 @@ def load_docs(path:str):
         loader = UnstructuredPDFLoader(path)
         return loader.load()
     except Exception:
-        logger.error("Error loading document")
+        return None
+    
+#function to create pinecone vector dataabase for RAG
+def create_pinecone_index(
+        index_name:str,
+        pinecone_key:str
+):
+    try:
+        from pinecone import Pinecone, ServerlessSpec
+        pc = Pinecone(api_key=pinecone_key)
+        if index_name not in pc.list_indexes().names():
+            pc.create_index(
+                name=index_name,
+                dimension=1536, # Replace with your model dimensions
+                metric="cosine", # Replace with your model metric
+                spec=ServerlessSpec(
+                    cloud="aws",
+                    region="us-east-1"
+                ) 
+            )
+    except Exception:
+        return None
+
+#create embeddings from nomic-embed-text model
+def create_embeddings(docs):
+    try:
+        from langchain_community.embeddings import OllamaEmbeddings
+        from langchain_text_splitters import RecursiveCharacterTextSplitter
+        # Split and chunk 
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=7500, chunk_overlap=100)
+        chunks = text_splitter.split_documents(docs)
+        ollama_emb = OllamaEmbeddings(
+            model="nomic-embed-text"
+        )
+        embeddings = ollama_emb.embed_documents(chunks)
+        return embeddings
+    
+    except Exception:
+        return None
+
+#add embeddings to pinecone vector database
+def add_embeddings_to_pinecone(
+        embeddings,
+        index_name:str,
+        namespace:str
+):
+    try:
+        from langchain_pinecone import PineconeVectorStore
+        vectorstore = PineconeVectorStore(
+            index_name=index_name,
+            namespace=namespace,
+            embedding=embeddings
+        )
+        return vectorstore
+    except Exception:
+        return None    
+
+#query the vector database
+def query_vector_db(
+        vectorstore,
+        query:str
+):
+    try:
+        from langchain_pinecone import PineconeSearch
+        search = PineconeSearch.from_vectorstore(vectorstore)
+        results = search.similarity_search(query, k=5)
+        return results
+    except Exception:
         return None
     
 
